60e4a4eeb20b47cd95d850f9f2e0f80ddd6703c2
Raul Macias <rmacias_c@hotmail.com>
2023-11-02 22:35:56 -0400

    feat: Added logger in main_sentiment.py

--- main_sentiment.py
+++ main_sentiment.py
@@ -1,5 +1,6 @@
 from src.sentiment import load_model, read_from_database, add_bad_words, add_sentiment, remove_moderators_comments_df
 from src.utils.df_saver import postgres_db
+from src.utils.logs import logger
 
 
 def main():
@@ -10,13 +11,14 @@ def main():
     df_comm, df_sub = read_from_database()
 
     df_sub = add_sentiment(df_sub, "submission", model, tokenizer)
-
-    df_comm = remove_moderators_comments_df(df_comm)
-    df_comm = add_sentiment(df_comm, "comment", model, tokenizer)
-    df_comm = add_bad_words(df_comm)
-
     storage.save_df(df_sub, "staging", "submissions")
-    storage.save_df(df_comm, "staging", "comments")
+    logger.info(f"{df_sub.shape[1]} submissions successfully saved.")
+    if not df_comm.is_empty():
+        df_comm = remove_moderators_comments_df(df_comm)
+        df_comm = add_sentiment(df_comm, "comment", model, tokenizer)
+        df_comm = add_bad_words(df_comm)
+        storage.save_df(df_comm, "staging", "comments")
+        logger.info(f"{df_comm.shape[1]} comments successfully saved.")
 
 
 if __name__ == "__main__":
